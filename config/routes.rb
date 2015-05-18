35f9abd7efad9ebf4940aa2ad2090feff3afd4f8
Joe Coleman <phantom42@gmail.com>
2015-05-18 12:02:08 -0400

    built out functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   resources :congoers
+  get 'bingo' => 'bingo#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'bingo#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
