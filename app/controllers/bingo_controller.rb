bdc718dcc35d5e8854497a4a345c35bf9f74baa1
Joe Coleman <phantom42@gmail.com>
2015-05-22 00:52:56 -0400

    added basic bootstrap navbar, fixed some styling

--- app/controllers/bingo_controller.rb
+++ app/controllers/bingo_controller.rb
@@ -11,13 +11,10 @@ class BingoController < ApplicationController
       if i == 13 
         @random_congoers << 'Free'
       else 
-        puts "counter: #{i}"
         congoer_index = rand(@all_congoers.length)
-        puts congoer_index
         congoer = @all_congoers[congoer_index]
         @random_congoers << congoer.description
         @all_congoers.delete_at(congoer_index)
-        puts "remaining:  #{@all_congoers.length}"
       end
     end
   end
