7bfe98fdbf5b8d73c7e23c09449dc13a9f7904b8
Joe Coleman <phantom42@gmail.com>
2015-06-15 23:38:29 -0400

    fixed code to not break if no data exists

--- app/controllers/bingo_controller.rb
+++ app/controllers/bingo_controller.rb
@@ -7,17 +7,19 @@ class BingoController < ApplicationController
   def load_congoers
     @all_congoers = Congoer.all.to_a
     @random_congoers = []
-    for i in 1..25
-      if i == 13 
-        @random_congoers << 'Free'
-      else 
-        puts "counter: #{i}"
-        congoer_index = rand(@all_congoers.length)
-        puts congoer_index
-        congoer = @all_congoers[congoer_index]
-        @random_congoers << congoer.description
-        @all_congoers.delete_at(congoer_index)
-        puts "remaining:  #{@all_congoers.length}"
+    if !@all_congoers.empty?
+      for i in 1..25
+        if i == 13 
+          @random_congoers << 'Free'
+        else 
+          puts "counter: #{i}"
+          congoer_index = rand(@all_congoers.length)
+          puts congoer_index
+          congoer = @all_congoers[congoer_index]
+          @random_congoers << congoer.description
+          @all_congoers.delete_at(congoer_index)
+          puts "remaining:  #{@all_congoers.length}"
+        end
       end
     end
   end
