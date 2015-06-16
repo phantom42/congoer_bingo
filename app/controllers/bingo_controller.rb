000e60e67985912e9815e4a68081311959045c0e
Joe Coleman <phantom42@gmail.com>
2015-06-16 00:05:13 -0400

    store card in session var

--- app/controllers/bingo_controller.rb
+++ app/controllers/bingo_controller.rb
@@ -6,21 +6,26 @@ class BingoController < ApplicationController
   private
   def load_congoers
     @all_congoers = Congoer.all.to_a
-    @random_congoers = []
-    if !@all_congoers.empty?
-      for i in 1..25
-        if i == 13 
-          @random_congoers << 'Free'
-        else 
-          puts "counter: #{i}"
-          congoer_index = rand(@all_congoers.length)
-          puts congoer_index
-          congoer = @all_congoers[congoer_index]
-          @random_congoers << congoer.description
-          @all_congoers.delete_at(congoer_index)
-          puts "remaining:  #{@all_congoers.length}"
+    if session[:congoers].nil?
+      @random_congoers = []
+      if !@all_congoers.empty?
+        for i in 1..25
+          if i == 13 
+            @random_congoers << 'Free'
+          else 
+            puts "counter: #{i}"
+            congoer_index = rand(@all_congoers.length)
+            puts congoer_index
+            congoer = @all_congoers[congoer_index]
+            @random_congoers << congoer.description
+            @all_congoers.delete_at(congoer_index)
+            puts "remaining:  #{@all_congoers.length}"
+          end
         end
+        session[:congoers] = @random_congoers
       end
+    else
+      @random_congoers = session[:congoers]
     end
   end
 end
